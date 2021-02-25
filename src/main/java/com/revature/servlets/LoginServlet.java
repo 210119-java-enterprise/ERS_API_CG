3b84e2932252f08fd561087320ada4a0b998f82d
Gabrielle Luna <lunagabrielled@gmail.com>
2021-02-24 16:39:26 -0800

    Login send JSON form

--- src/main/java/com/revature/servlets/LoginServlet.java
+++ src/main/java/com/revature/servlets/LoginServlet.java
@@ -35,23 +35,10 @@ public class LoginServlet extends HttpServlet {
 
         resp.setContentType("text/html");
         PrintWriter printWriter = resp.getWriter();
-        printWriter.print("<html>");
-        printWriter.print("<body>");
-        printWriter.print("<div style=\"text-align: center\">\n" +
-                "        <h1>ERS Login</h1>\n" +
-                "        <form action=\"login\" method=\"post\">\n" +
-                "            <label for=\"username\">Username:</label>\n" +
-                "            <input name=\"username\" size=\"30\" />\n" +
-                "            <br><br>\n" +
-                "            <label for=\"password\">Password:</label>\n" +
-                "            <input type=\"password\" name=\"password\" size=\"30\" />\n" +
-                "            <br>${message}\n" +
-                "            <br><br>           \n" +
-                "            <button type=\"submit\">Login</button>\n" +
-                "        </form>\n" +
-                "    </div>");
-        printWriter.print("</body>");
-        printWriter.print("</html>");
+        printWriter.print("{\n" +
+                "    \"username\": \" \",\n" +
+                "    \"password\": \" \",\n" +
+                "}");
         printWriter.close();
 
     }
