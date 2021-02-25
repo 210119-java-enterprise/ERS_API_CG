09278e7b7633a1f51641c409bd57e00bb16d4d95
Gabrielle Luna <lunagabrielled@gmail.com>
2021-02-25 08:00:05 -0800

    passing reimbursement

--- src/main/java/com/revature/servlets/ReimbursementServlet.java
+++ src/main/java/com/revature/servlets/ReimbursementServlet.java
@@ -1,5 +1,7 @@
 package com.revature.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.User;
 import com.revature.services.ReimbursementService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,17 +11,52 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "reimbursements", displayName = "reimbursements", urlPatterns = "/reimbursements/*")
 public class ReimbursementServlet extends HttpServlet {
 
     private final ReimbursementService reimbursementService = new ReimbursementService();
-    private static final Logger logger = LogManager.getLogger(ReimbursementServlet.class);
+    private static final Logger LOG = LogManager.getLogger(ReimbursementServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //super.doGet(req, resp);
+        PrintWriter writer = resp.getWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        HttpSession session = req.getSession(false);
+        User requester = session == null? (User) req.getSession(false).getAttribute("this-user");
+        resp.setContentType("application/json");
+
+        //Need a reimbursementId
+        //String userIdParam = req.getParameter("userId");
+
+        try{
+            //Must be Finance Manager
+            if (requester != null && requester.getUserRole().compareTo(2) == 0){
+                LOG.info("UserServlet.doGet() invoked by requester {}", requester);
+
+
+
+            }else{
+                if (requester == null){
+                    //User got past login or using invalidated session
+                    LOG.warn("Unauthorized request made by unknown requester");
+                    resp.setStatus(401);
+                }else{
+                    //User is not a Finance Manager/authorized user
+                    LOG.warn("Request made by requester, {}, who lacks proper authorities", requester.getUsername());
+                    resp.setStatus(403);
+                }
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+            LOG.error(e.getMessage());
+            resp.setStatus(500);
+        }
+
     }
 
     @Override
